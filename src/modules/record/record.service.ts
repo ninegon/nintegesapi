5a24f65e4d99795d0ff999f42d2081198c8f0a88
ninegon <114958334+ninegon@users.noreply.github.com>
2023-07-10 23:05:28 +0200

    Update record.service.ts

--- src/modules/record/record.service.ts
+++ src/modules/record/record.service.ts
@@ -12,7 +12,7 @@ export class RecordService extends BaseService {
         this.recordRepo = this.makeQB(RecordEntity, 'r')
     }
 
-    allRecords = async (): Promise<RecordEntity[]> => await this.recordRepo.getMany()
+    allRecords = async (): Promise<RecordEntity[]> => await this.recordRepo.getMany().orderby({ id },"desc")
     insertRecord = async (type: number, userId: number, workplaceId: number, isSameLocation: boolean, note: string): Promise<InsertResult> => await this.recordRepo.insert().into(RecordEntity).values({ type, userId, workplaceId, isSameLocation, note }).execute()
     deleteRecord = async (id: number): Promise<DeleteResult> => await this.recordRepo.delete().where({ id }).execute()
 }

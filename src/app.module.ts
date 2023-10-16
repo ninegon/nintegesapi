c62e254cf91813569ac1fa10bbf5f5b5a29a644d
Manuel <manolo.meniba@gmail.com>
2023-10-16 22:57:08 +0200

    build hecho

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,16 @@ import { UserModule } from './modules/user/user.module';
 import { TenancyProvider } from './core/tenancyProvider';
 import { WorkplaceModule } from './modules/workplace/workplace.module';
 import { RecordModule } from './modules/record/record.module';
+import { WorkplaceCalendarModule } from './modules/workplaceCalendar/workplaceCalendar.module';
 
 @Module({
   imports: [
     UserModule,
     WorkplaceModule,
+    WorkplaceCalendarModule,
     RecordModule,
     ConfigModule.forRoot({ isGlobal: true }),
     TenancyProvider
   ],
 })
-export class AppModule {}
+export class AppModule { }
--- src/modules/workplaceCalendar/workplaceCalendar.service.ts
+++ src/modules/workplaceCalendar/workplaceCalendar.service.ts
@@ -13,7 +13,7 @@ export class WorkplaceCalendarService extends BaseService {
         this.userWorkplaceCalendar = this.makeQB(WorkplaceCalendarEntity, 'r')
     }
 
-    allWorkplaceCalendars = async (): Promise<WorkplaceCalendarEntity[]> => await this.workplaceCalendarRepo.getMany()
+    allWorkplaceCalendars = async (): Promise<WorkplaceCalendarEntity[]> => await this.workplaceCalendarRepo.leftJoinAndSelect('r.workplace', 'workplace').getMany()
     insertWorkplaceCalendar = async (workplaceId: number, hours: number, date: Date): Promise<InsertResult> => await this.workplaceCalendarRepo.insert().into(WorkplaceCalendarEntity).values({ workplaceId, hours, date }).execute()
     deleteWorkplaceCalendar = async (id: number): Promise<DeleteResult> => await this.workplaceCalendarRepo.delete().where({ id }).execute()
 
